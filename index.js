3d9053995a82aac940908a0658b34151b9bdee4a
Lawrence J <lawrenceg563@gmail.com>
2021-09-17 10:09:56 +0530

    created api to create a file named current time and date with time stamp as content

--- index.js
+++ index.js
@@ -3,30 +3,42 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
+const p = (fname)=> (path.join(
+    path.dirname(require.main.filename),
+    "data",
+    fname
+  ))
 
-//   res.send('ok');
-
+const allFiles = (cb) => {
+    fs.readdir(p, (err, fileContent) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(fileContent)
+      }
+    });
+  };
 
 app.get('/',(req,res)=>{
+    fs.readdir(path, callbackFunction) 
+})
+
+app.get('/create-file',(req,res)=>{
     
     const timeStamp = Date.now();
     const date = new Date(timeStamp)
     const fname = date.getDate()+
-    "/"+(date.getMonth()+1)+
-    "/"+date.getFullYear()+
+    "-"+(date.getMonth()+1)+
+    "-"+date.getFullYear()+
     " "+date.getHours()+
-    ":"+date.getMinutes()+
-    ":"+date.getSeconds();
+    "."+date.getMinutes()+
+    "."+date.getSeconds()+".txt";
+   
+    console.log(p(fname))
 
-    const p = path.join(
-        path.dirname(require.main.filename),
-        "data",
-        "mentors.json"
-      );
-      console.log(p)
-    
-    console.log(fname)
-    res.send("ok")
+      fs.writeFile(p(fname), timeStamp.toString(), (err) => {console.log(err)});
+
+    res.json({message:`created file ${fname}`})
 })
 
 app.listen('5000',()=>console.log("server listerning to port 5000"))
